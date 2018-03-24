4037363679c537ab13db9a17184b7ffad997fb24
Malik Albeik <abd.beik@me.com>
2018-03-24 17:52:07 +0300

    created the createCORSRequest function make cors requests

--- src/component/App.js
+++ src/component/App.js
@@ -22,6 +22,27 @@ class App extends Component {
       alert("script not loaded")
     }
   }
+  // Used the tutorial in https://www.html5rocks.com/en/tutorials/cors/ to create a CORS request function.
+  createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+      // Check if the XMLHttpRequest object has a "withCredentials" property.
+      // "withCredentials" only exists on XMLHTTPRequest2 objects.
+      xhr.open(method, url, true);
+    } else if (typeof XDomainRequest !== "undefined") {
+      // Otherwise, check if XDomainRequest.
+      // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+      xhr = new XDomainRequest();
+      xhr.open(method, url);
+
+    } else {
+      // Otherwise, CORS is not supported by the browser.
+      xhr = null;
+    }
+    return xhr;
+  }
+
+
   render() {
     return (
       <div id="container">
