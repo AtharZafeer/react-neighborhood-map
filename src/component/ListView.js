da4ec522a3889421c45a0abdde465c22d9813365
Malik Albeik <abd.beik@me.com>
2018-03-25 14:44:18 +0300

    Added listview to list the places' names and controls the search input of the user

---
+++ src/component/ListView.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class ListView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    };
+  }
+  render() {
+    return (
+
+    )
+  }
+}
+
+export default ListView;
